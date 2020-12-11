686de31e171691bc91e4d06cd380b20cf2eb7ca2
ZhouJianzj <2998390669@qq.com>
2020-12-11 23:21:11 +0800

    Thread 1、进程线程的概述 2、如何创建一个线程 3、分支线程的堆内存和方法区内存是共享的 4、每一个分支都单独的对应一个栈，线程的执行的时候是一起执行的。 5、start（）；启动线程就是表示线程就绪状态，也就是只是执行开辟分支栈的功能 6、JVM会执行线程机制去压栈Run（）方法为分支栈的栈底针，等同于main（）

---
+++ ZhouJian/src/main/java/ThreadStudy/MyThreadTest.java
@@ -0,0 +1,17 @@
+package ThreadStudy;
+
+/**
+ * @ClassName MyThread
+ * @Description TODO
+ * @Author ZhouJian
+ * @Date 2020/12/11
+ **/
+public class MyThreadTest extends Thread {
+    @Override
+    public void run() {
+
+     for (int a = 0;a < 1000; a++){
+         System.out.println("分支线程执行！FFFFFFFF");
+     }
+    }
+}
---
+++ ZhouJian/src/main/java/ThreadStudy/ThreadTest.java
@@ -0,0 +1,37 @@
+package ThreadStudy;
+
+/**
+ * @ClassName ThreadTest
+ * @Description TODO
+ *                      线程的学习（是一个执行单元）
+ *                          1、进程是大哥线程是小弟
+ *                          2、一个线程对应一个独立的栈
+ *                             涉及到多线程并发
+ *                          3、程序执行的时候JVM最少执行两个线程
+ *                          4、多线程加快执行速度，提高程序的效率
+ *                          5、main方法线程结束了，不不代表程序的结束
+ *                             还有可能别的线程在进行弹栈压栈的操作
+ * @Author ZhouJian
+ * @Date 2020/12/11
+ **/
+public class ThreadTest {
+    public static void main(String[] args) {
+//        创建一个分支线程
+        MyThreadTest myThreadTest = new MyThreadTest();
+//        启动分支线程
+        myThreadTest.start();
+//        这里强调一下子使用下面的方式执行不会触发多线的只是调用了方法而已
+//        myThreadTest.run();
+
+/**
+ * 在执行的时候会发现先是执行的sout然后才执行分支线程的
+ *     道理还是那个道理自上而下的执行
+ *     只是start()执行了，表示开辟分支栈
+ *     两个栈式并行的一起执行的
+ */
+
+        for (int t = 0;t < 1000; t++) {
+            System.out.println("main线程在执行！MMMMMMMMMMMMMM");
+        }
+    }
+}
