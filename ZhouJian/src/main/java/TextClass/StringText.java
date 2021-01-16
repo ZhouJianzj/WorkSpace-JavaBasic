507787015c434111fbffed8d1d920a81d6f2eb2e
ZhouJianzj <2998390669@qq.com>
2021-01-16 22:23:58 +0800

    复习准备考试

--- ZhouJian/src/main/java/TextClass/StringText.java
+++ ZhouJian/src/main/java/TextClass/StringText.java
@@ -2,6 +2,8 @@ package TextClass;
 
 import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 
+import java.util.Comparator;
+
 /**
  * @ClassName StringText
  * @Description TODO
@@ -9,13 +11,4 @@ import com.mysql.cj.x.protobuf.MysqlxDatatypes;
  * @Date 2021/1/15
  **/
 public class StringText {
-    public static void main(String[] args) {
-        String s = new String("zhouajian");
-        //返回字符串的长度
-        int length = s.length();
-        System.out.println( length +"++++++++++++++++++++++++++");
-        //返回的是Boolean类型
-        System.out.println(s.equals("zhoujian"));
-
-    }
 }
